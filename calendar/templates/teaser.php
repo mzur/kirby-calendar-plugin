5901ba2297cfc27a32460430fb146dcf0de8cd8b
mzur <mouagip@nexus-der-macht.de>
2012-12-26 19:56:49 +0100

    Refactoring for nicer code output.

--- calendar/templates/teaser.php
+++ calendar/templates/teaser.php
@@ -5,33 +5,28 @@
 ?>
 
 <ul class="teaser">
-	<?php 
-		//number of events to show
-		$max = 3;
-		$count = 0;
-	?>
-	<?php foreach ($this->events as $event): ?>
-	<?php
-		if ($count >= $max) break;
-		$begin = $event->getBegin();
-		$end = $event->getEnd();
-		
-		//skip all past events
-		if (($end && $end[0] >= time()) || $begin[0] >= time()):
-			$count++;
-	?>
-			<li<?php echo ($count>=$max) ? ' class="last"' : '';?>>
-			
-				<time datetime="<?php echo gmdate("Y-m-d\TH:i:s\Z", $begin[0]);?>">
-					<?php echo strftime($this->dateFormat, $begin[0]); ?>
-					<?php echo ($end)? ' - '.strftime($this->dateFormat, $end[0]) : ''; ?>
-				</time>
-				
-				<?php foreach ($columnsArr as $column): $info = $event->getInfo(); ?>
-					<?php echo (array_key_exists($column, $info))? $info[$column] : ''; ?>
-				<?php endforeach; ?>
-				
-			</li>
-		<?php endif; ?>	
-	<?php endforeach; ?>
+<?php 
+//number of events to show
+$max = 3;
+$count = 0;
+
+foreach ($this->events as $event):
+
+	if ($count >= $max) break;
+	$begin = $event->getBegin();
+	$end = $event->getEnd();
+	
+	//skip all past events
+	if (($end && $end[0] >= time()) || $begin[0] >= time()):
+		$count++;
+?>
+	<li<?php echo ($count>=$max) ? ' class="last"' : '';?>>
+		<time datetime="<?php echo gmdate("Y-m-d\TH:i:s\Z", $begin[0]);?>"><?php echo strftime($this->dateFormat, $begin[0]); ?><?php echo ($end)? ' - '.strftime($this->dateFormat, $end[0]) : ''; ?></time>
+<?php foreach ($columnsArr as $column): $info = $event->getInfo(); ?>
+		<?php echo (array_key_exists($column, $info))? $info[$column] : ''; ?>
+<?php endforeach; ?>
+	
+	</li>
+<?php endif; ?>
+<?php endforeach; ?>
 </ul>
