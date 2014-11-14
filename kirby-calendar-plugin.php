73db6f1e42223a7c2e32c355b74b211fec760edc
Martin Zurowietz <martin@zurowietz.de>
2014-11-14 16:19:24 +0100

    Renamed main file so the plugin works with Kirby.

--- kirby-calendar-plugin.php
+++ kirby-calendar-plugin.php
@@ -3,7 +3,6 @@ if(!class_exists('Calendar'))  require_once('lib/Calendar.php');
 if(!class_exists('Event')) require_once('lib/Event.php');
 
 function calendar($events, $options=array(), $template='table') {
-		$calendar = new Calendar($events, $options);
-		echo (empty($events)) ? $calendar->noEntry() : $calendar->cal($template);
+	$calendar = new Calendar($events, $options);
+	echo (empty($events)) ? $calendar->noEntry() : $calendar->cal($template);
 }
-?>
